e191bd4f92498354a43c846d1e484cff5d20cfb0
Strahinja Val Markovic <val@markovic.io>
2014-03-06 21:46:25 -0800

    Added a function for parsing codepoint from &[u8]

---
+++ parser/unicode.rs
@@ -0,0 +1,127 @@
+use std::char;
+
+// More details: http://en.wikipedia.org/wiki/UTF-8#Description
+static UTF8_1BYTE_FOLLOWING: u8 = 0b11000000;
+static UTF8_2BYTE_FOLLOWING: u8 = 0b11100000;
+static UTF8_3BYTE_FOLLOWING: u8 = 0b11110000;
+
+pub fn readCodepoint( input: &[u8] ) -> Option< char > {
+  fn bytesFollowing( byte: u8 ) -> Option< uint > {
+    if byte & 0b11100000 == UTF8_1BYTE_FOLLOWING {
+      return Some( 1 );
+    }
+    if byte & 0b11110000 == UTF8_2BYTE_FOLLOWING {
+      return Some( 2 );
+    }
+    if byte & 0b11111000 == UTF8_3BYTE_FOLLOWING {
+      return Some( 3 );
+    }
+    return None;
+  }
+
+  fn isAscii( byte: u8 ) -> bool {
+    return byte & 0b10000000 == 0;
+  }
+
+  fn isContinuationByte( byte: u8 ) -> bool {
+    byte & 0b11000000 == 0b10000000
+  }
+
+  fn codepointBitsFromLeadingByte( byte: u8 ) -> u32 {
+    let good_bits =
+      if byte & 0b11100000 == UTF8_1BYTE_FOLLOWING {
+        byte & 0b00011111
+      } else if byte & 0b11110000 == UTF8_2BYTE_FOLLOWING {
+        byte & 0b00001111
+      } else {
+        byte & 0b00000111
+      };
+    good_bits as u32
+  }
+
+  fn codepointBitsFromContinuationByte( byte: u8 ) -> u32 {
+    ( byte & 0b00111111 ) as u32
+  }
+
+  match input.get( 0 ) {
+    Some( first_byte ) if isAscii( *first_byte ) => {
+      char::from_u32( *first_byte as u32 )
+    }
+
+    Some( first_byte ) => {
+      match bytesFollowing( *first_byte ) {
+        Some( num_following ) => {
+          let mut codepoint: u32 =
+            codepointBitsFromLeadingByte( *first_byte ) << 6 * num_following;
+          for i in range( 1, num_following + 1 ) {
+            match input.get( i ) {
+              Some( byte ) if isContinuationByte( *byte ) => {
+                codepoint |= codepointBitsFromContinuationByte( *byte ) <<
+                  6 * ( num_following - i );
+              }
+              _ => return None
+            }
+          }
+          char::from_u32( codepoint )
+        }
+        _ => None
+      }
+    }
+    _ => None
+  }
+}
+
+
+pub fn charToUtf8( input: char ) -> ~[u8] {
+  let utf8chars: &mut [u8] = [0, ..4];
+  let num_written = input.encode_utf8( utf8chars );
+  let mut out: ~[u8] = ~[];
+  for i in range( 0, num_written ) {
+    out.push( *utf8chars.get( i ).unwrap() );
+  }
+  out
+}
+
+
+#[cfg(test)]
+mod tests {
+  use super::{readCodepoint, charToUtf8, UTF8_1BYTE_FOLLOWING};
+
+  #[test]
+  fn readCodepoint_Roundtrip_SimpleAscii() {
+    assert_eq!( 'a', readCodepoint( bytes!( 'a' ) ).unwrap() );
+    assert_eq!( 'z', readCodepoint( bytes!( 'z' ) ).unwrap() );
+    assert_eq!( 'A', readCodepoint( bytes!( 'A' ) ).unwrap() );
+    assert_eq!( '9', readCodepoint( bytes!( '9' ) ).unwrap() );
+    assert_eq!( '*', readCodepoint( bytes!( '*' ) ).unwrap() );
+    assert_eq!( '\n', readCodepoint( bytes!( '\n' ) ).unwrap() );
+    assert_eq!( '\0', readCodepoint( bytes!( '\0' ) ).unwrap() );
+  }
+
+
+  #[test]
+  fn readCodepoint_Roundtrip_NonAscii() {
+    // 2 UTF-8 bytes
+    assert_eq!( '¢', readCodepoint( charToUtf8( '¢' ) ).unwrap() );
+
+    // 3 UTF-8 bytes
+    assert_eq!( '€', readCodepoint( charToUtf8( '€' ) ).unwrap() );
+
+    // 4 UTF-8 bytes
+    assert_eq!( '𤭢', readCodepoint( charToUtf8( '𤭢' ) ).unwrap() );
+
+    // Some extras
+    assert_eq!( 'Ć', readCodepoint( charToUtf8( 'Ć' ) ).unwrap() );
+    assert_eq!( 'Ө', readCodepoint( charToUtf8( 'Ө' ) ).unwrap() );
+    assert_eq!( '自', readCodepoint( charToUtf8( '自' ) ).unwrap() );
+    assert_eq!( '由', readCodepoint( charToUtf8( '由' ) ).unwrap() );
+  }
+
+
+  #[test]
+  fn readCodepoint_FailsOnBadChars() {
+    assert!( readCodepoint( [ 0b11111111 ] ).is_none() );
+    assert!( readCodepoint( [ 0b10000000 ] ).is_none() );
+    assert!( readCodepoint( [ UTF8_1BYTE_FOLLOWING, 0b11000000 ] ).is_none() );
+  }
+}
