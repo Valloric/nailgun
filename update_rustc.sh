30ce43f58b16206de4afe2151c1def91ec65a081
Strahinja Val Markovic <val@markovic.io>
2014-08-22 20:32:41 -0700

    Using new location for rustup.sh

--- update_rustc.sh
+++ update_rustc.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Downloads new rust binary package, uninstalls the old one, installs new one
-curl -s http://www.rust-lang.org/rustup.sh | sudo sh
+curl -s https://static.rust-lang.org/rustup.sh | sudo sh
